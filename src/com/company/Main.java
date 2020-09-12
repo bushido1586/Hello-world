cc586375ba919e1ed7cdd33eb849702cfe6e8dd9
Brad Reed <67254003+bushido1586@users.noreply.github.com>
2020-09-12 19:18:31 -0400

    enhanced for loop, array split example Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,11 +4,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        int count = 5;
-        do {
-            System.out.println(count);
-            System.out.println("Hello There!");
-            count--;
-        }while(count>0);
+        int[] number = {1,2,3,4,5,6,7,8,9,10};
+        int[] odd = new int[5];
+        int[] even = new int[5];
+        int i=0;
+        int j =0;
+
+        for(int a: number){
+            if(a %2 == 0){
+                even[i]=a;
+                i++;
+            }else{
+                odd[j]=a;
+                j++;
+            }
+        }
+        for(int x : even){
+            System.out.println(x);
+        }System.out.println(" ");
+        for(int y :odd){
+            System.out.println(y);
+        }
     }
 }
