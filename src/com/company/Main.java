3dd1c554eed683a9dd308309ba3109a308800205
Brad Reed <67254003+bushido1586@users.noreply.github.com>
2020-09-12 16:47:27 -0400

    do-while loop conditional example Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,14 @@
 package com.company;
 
-import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
-        int num = 4;
-        switch (num){
-            case 1: System.out.println("This is one");
-            break;
-            case 2: System.out.println("This is two");
-            break;
-            case 3: System.out.println("This is three");
-            break;
-            case 4: System.out.println("This is four");
-            break;
-            case 5: System.out.println("This is five");
-            break;
-            default: System.out.println("The number is out of range");
-            break;
-            //much cleaner than if/else-if statements for large number of conditions.
-        }
+        int count = 5;
+        do {
+            System.out.println(count);
+            System.out.println("Hello There!");
+            count--;
+        }while(count>0);
     }
 }
