d72de663ed68781053682902ca2f8062a3e191e4
Brad Reed <67254003+bushido1586@users.noreply.github.com>
2020-09-14 20:46:51 -0400

    switch-case practice example Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,15 +7,25 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        System.out.println("Please enter your score.");
+    public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int x= Integer.parseInt(reader.readLine());
+        int day = Integer.parseInt(reader.readLine());
         reader.close();
-        if(x>=35){
-            System.out.println("You passed!");
-        }else if (x<35){
-            System.out.println("You failed");
+        switch(day){
+            case 1: System.out.println("Monday");
+            break;
+            case 2 : System.out.println("Tuesday");
+            break;
+            case 3 : System.out.println("Wednesday");
+            break;
+            case 4: System.out.println("Thursday");
+            break;
+            case 5 : System.out.println("Friday");
+            break;
+            case 6 : System.out.println("Saturday");
+            break;
+            case 7 : System.out.println("Sunday");
+            break;
         }
     }
 }
