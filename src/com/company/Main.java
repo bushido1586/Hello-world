b0cb77edb40d6c49dfc139c227f4f82f26eb7c65
Brad Reed <67254003+bushido1586@users.noreply.github.com>
2020-09-14 21:01:02 -0400

    multiplication table creator example Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,30 +2,17 @@ package com.company;
 
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int day = Integer.parseInt(reader.readLine());
+        System.out.println("Please enter a number:");
+        int num = Integer.parseInt(reader.readLine());
         reader.close();
-        switch(day){
-            case 1: System.out.println("Monday");
-            break;
-            case 2 : System.out.println("Tuesday");
-            break;
-            case 3 : System.out.println("Wednesday");
-            break;
-            case 4: System.out.println("Thursday");
-            break;
-            case 5 : System.out.println("Friday");
-            break;
-            case 6 : System.out.println("Saturday");
-            break;
-            case 7 : System.out.println("Sunday");
-            break;
+        for(int i=0; i<11; i++){
+            System.out.println(i*num);
         }
     }
 }
