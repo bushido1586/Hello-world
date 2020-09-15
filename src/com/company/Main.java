9605f67876db27fd59cf4e61781c1cec9f78bd9f
Brad Reed <67254003+bushido1586@users.noreply.github.com>
2020-09-14 21:18:24 -0400

    scanner to array using loop example Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,26 +1,21 @@
 package com.company;
 
 
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        int[] ticket_price ={100,150,500,20};
-        int[] discounted_price = new int[ticket_price.length];
-        int new_price;
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int discount = Integer.parseInt(reader.readLine());
-        reader.close();
-
-        for(int i=0; i< ticket_price.length; i++){
-            new_price = (ticket_price[i] - (ticket_price[i]* discount)/100);
-            discounted_price[i] = new_price;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("How many values do you wish to enter:");
+        int n = sc.nextInt();
+        int[] values= new int[n];
+        for(int i=0; i<n; i++){
+            values[i]=sc.nextInt();
         }
-        for (int i : discounted_price) {
-            System.out.println(i);
+        System.out.println("Your entered values are: ");
+        for(int j= 0; j<n; j++){
+            System.out.print(values[j]+" ");
         }
-
     }
 }
