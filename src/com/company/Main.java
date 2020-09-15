bb2bb0d886fec7e8d32bea48e3e25bedde094b5b
Brad Reed <67254003+bushido1586@users.noreply.github.com>
2020-09-15 15:55:14 -0400

    this keyword practice example Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,19 +6,10 @@ public class Main {
     public static void main(String[] args) {
 
         //creating object
-        double rob_marks;
-        double sum;
 
-        Student rob = new Student();
-        rob.id = 1;
-        rob.chem = 67;
-        rob.phy = 98;
-        rob.math = 84;
-
-        sum=rob.total();
-        System.out.println(sum);
-        rob_marks = rob.percentage(sum);
-        System.out.println(rob_marks);
-
+        Student rob = new Student(1,60,76,89);
+        System.out.println(rob.phy);
+        Student john = new Student(2,68, 65, 90);
+        System.out.println(john.math);
     }
 }
--- src/com/company/Student.java
+++ src/com/company/Student.java
@@ -6,6 +6,14 @@ public class Student {
     double chem;
     double math;
 
+    Student(int id, double phy, double chem, double math){
+        this.id=id;
+        this.phy=phy;
+        this.chem=chem;
+        this.math=math;
+        //this refers to the class variable instead of the parameter variable.
+    }
+
     double percentage(double sum){
         return (sum/3);
     }
