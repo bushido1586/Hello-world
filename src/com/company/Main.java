2905a6dfad02b1376a08bfac5cc21b78bd2c0d0a
Brad Reed <67254003+bushido1586@users.noreply.github.com>
2020-09-14 21:08:15 -0400

    discount calculator(array) example Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,12 +7,20 @@ import java.io.InputStreamReader;
 public class Main {
 
     public static void main(String[] args) throws Exception {
+        int[] ticket_price ={100,150,500,20};
+        int[] discounted_price = new int[ticket_price.length];
+        int new_price;
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Please enter a number:");
-        int num = Integer.parseInt(reader.readLine());
+        int discount = Integer.parseInt(reader.readLine());
         reader.close();
-        for(int i=0; i<11; i++){
-            System.out.println(i*num);
+
+        for(int i=0; i< ticket_price.length; i++){
+            new_price = (ticket_price[i] - (ticket_price[i]* discount)/100);
+            discounted_price[i] = new_price;
         }
+        for (int i : discounted_price) {
+            System.out.println(i);
+        }
+
     }
 }
