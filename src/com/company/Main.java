fae8725af10ebbe2747fac74644b72ecec4e3065
Brad Reed <67254003+bushido1586@users.noreply.github.com>
2020-09-24 17:08:59 -0400

    Creating exception solution Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,12 +4,15 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Employee a = new Employee();
-        a.setData();
-        a.getData();
+        int age = 15;
 
-        Tech t = new Tech();
-        t.getSalary();
-
+        try{
+            if(age<18){
+                throw new NotAnAdultException();
+            }
+        }catch (NotAnAdultException e){
+            System.out.println(e);
+        }
     }
+
 }
---
+++ src/com/company/NotAnAdultException.java
@@ -0,0 +1,8 @@
+package com.company;
+//creating our own exceptions
+public class NotAnAdultException extends Exception {
+
+    public String toString(){
+        return ("Not an Adult");
+    }
+}
