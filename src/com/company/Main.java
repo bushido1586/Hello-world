9ad93a146d9b7025afec4470dc3d0ac958c4b367
Brad Reed <67254003+bushido1586@users.noreply.github.com>
2020-09-25 18:29:59 -0400

    problem 12 solution practice Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,18 +2,29 @@ package com.company;
 
 
 import java.io.*;
+import java.util.Scanner;
 
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        FileOutputStream f = new FileOutputStream("demo.txt");
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        System.out.println("Enter text for the file:");
-        String s = reader.readLine();
-        byte b[] =s.getBytes();
-        f.write(b);
-        f.close();
+        Scanner sc = new Scanner(new File("balance.txt"));
+        int currentBalance = sc.nextInt();
+        System.out.println(currentBalance);
+
+        Scanner sc1 = new Scanner(System.in);
+        System.out.println("Please enter deposit amount: ");
+        int deposit = sc1.nextInt();
+
+        currentBalance+=deposit;
+
+        System.out.println("Updated balance is: "+currentBalance);
+        FileOutputStream outstream = new FileOutputStream("balance.txt");
+        int bal = currentBalance;
+        String s = Integer.toString(bal);
+        byte b[]=s.getBytes();
+        outstream.write(b);
+        outstream.close();
     }
 
 }
