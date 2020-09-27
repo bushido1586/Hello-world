1d0ad9eb810fcb9d287ebbe4642ab3e386e8fccb
Brad Reed <67254003+bushido1586@users.noreply.github.com>
2020-09-27 16:24:43 -0400

    getChars() example Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,8 +5,19 @@ package com.company;
 public class Main {
 
     public static void main(String[] args)  {
-        String name = "John";
-        System.out.println(name.charAt(2));
+        String sentence ="This is some text";
+        /*void getChars(int sourceStart, int sourceEnd, char target, int targetStart)
+        enter value from where you want to start, end, target, and where it goes
+         */
+        int start =0;
+        int end = 10;
+        char tar[] = new char[end-start];
+
+        sentence.getChars(start, end, tar, 0);
+        System.out.println(tar);
+
+
+
 
     }
 
