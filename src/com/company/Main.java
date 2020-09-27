b350e28414c0e91f8b82d96f9c98d6b213113d15
Brad Reed <67254003+bushido1586@users.noreply.github.com>
2020-09-27 16:17:40 -0400

    charAt() practice Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,30 +1,13 @@
 package com.company;
 
 
-import java.io.*;
-import java.util.Scanner;
-
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(new File("balance.txt"));
-        int currentBalance = sc.nextInt();
-        System.out.println(currentBalance);
+    public static void main(String[] args)  {
+        String name = "John";
+        System.out.println(name.charAt(2));
 
-        Scanner sc1 = new Scanner(System.in);
-        System.out.println("Please enter deposit amount: ");
-        int deposit = sc1.nextInt();
-
-        currentBalance+=deposit;
-
-        System.out.println("Updated balance is: "+currentBalance);
-        FileOutputStream outstream = new FileOutputStream("balance.txt");
-        int bal = currentBalance;
-        String s = Integer.toString(bal);
-        byte b[]=s.getBytes();
-        outstream.write(b);
-        outstream.close();
     }
 
 }
