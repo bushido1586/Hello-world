83616137b377e4482accd82447a7fe32e5815c78
Brad Reed <67254003+bushido1586@users.noreply.github.com>
2020-09-27 19:41:06 -0400

    toCharArray() Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,20 +5,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args)  {
-        String sentence ="This is some text";
-        /*void getChars(int sourceStart, int sourceEnd, char target, int targetStart)
-        enter value from where you want to start, end, target, and where it goes
-         */
-        int start =0;
-        int end = 10;
-        char[] tar = new char[end-start];
 
-        sentence.getChars(start, end, tar, 0);
-        System.out.println(tar);
+        String sentence = "competition";
+        char[] c =sentence.toCharArray();
 
-
-
-
+        for (int i=0; i<sentence.length(); i++){
+            System.out.println(c[i]);
+        }
     }
 
 }
