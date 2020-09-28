75decf3b60ca23bb76383e145b2a2b201ca1747d
Brad Reed <67254003+bushido1586@users.noreply.github.com>
2020-09-28 17:18:18 -0400

    startsWith/edsWith Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,10 +9,9 @@ public class Main {
         String s1 = "Hello there";
         String s2 = "there";
 
-
-        //^ toffset where to start first ont, oofset where to start, second one, len starts at index 0
-
-        System.out.println(s1.regionMatches(6,s2,0,4));
+        //System.out.println(s1.regionMatches(6,s2,0,4));
+        System.out.println(s1.endsWith("ere"));
+        System.out.println(s2.startsWith("t"));
     }
 
 }
