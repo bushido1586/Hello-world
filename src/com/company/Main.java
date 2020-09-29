2e5a5fe805ed16f15b0ae5ccd8e3ef6a5f8c55a3
Brad Reed <67254003+bushido1586@users.noreply.github.com>
2020-09-28 20:57:05 -0400

    to upper/lowercase method Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,8 +6,10 @@ public class Main {
 
     public static void main(String[] args)  {
 
-        String s1 = "           Hello World       ";
-       System.out.println(s1.trim());
+        String s1 = "Hello World";
+        System.out.println(s1.toUpperCase());
+
+        System.out.println(s1.toLowerCase());
 
     }
 
