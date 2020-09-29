f41b8370f1a510773505645f1a0b7d8ce77ec06c
Brad Reed <67254003+bushido1586@users.noreply.github.com>
2020-09-28 21:01:12 -0400

    String.join() method Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -6,10 +6,11 @@ public class Main {
 
     public static void main(String[] args)  {
 
-        String s1 = "Hello World";
-        System.out.println(s1.toUpperCase());
+        String s1 = String.join(" : ", "red", "blue", "green");
+        System.out.println(s1);
+        String s2 = String.join("/", "12","11","2020");
+        System.out.println(s2);
 
-        System.out.println(s1.toLowerCase());
 
     }
 
