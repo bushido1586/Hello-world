be6f4558460e04fdccfe3d0fd1c8c01bf9932992
Brad Reed <67254003+bushido1586@users.noreply.github.com>
2020-09-29 20:05:44 -0400

    problem 15:username/password check  Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,16 +5,25 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args)  {
+        String username="admin";
+        String password="adminuser";
+        String enteredUsername;
+        String enteredPassword;
         Scanner sc = new Scanner(System.in);
-        System.out.println("please enter some text");
-        String s = sc.nextLine();
-        int count =0;
-        for(int i=0; i<s.length(); i++){
-            if(s.charAt(i)=='a'){
-                count++;
+        System.out.println("Enter username");
+        enteredUsername = sc.next();
+        if(username.equals(enteredUsername)){
+            System.out.println("Enter the password");
+            enteredPassword = sc.next();
+            if(password.equals(enteredPassword)){
+                System.out.println("Successfully logged in");
+            }else{
+                System.out.println("Incorrect password");
             }
+        }else{
+            System.out.println("Invalid username");
         }
-        System.out.println(count);
+
     }
 
 }
