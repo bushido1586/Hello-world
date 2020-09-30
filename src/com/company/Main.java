c92de07b5b8277716c98b3325cbf07eab2d9a00a
Brad Reed <67254003+bushido1586@users.noreply.github.com>
2020-09-30 16:24:27 -0400

    problem 16 solution  Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,25 +5,26 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args)  {
-        String username="admin";
-        String password="adminuser";
-        String enteredUsername;
-        String enteredPassword;
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter username");
-        enteredUsername = sc.next();
-        if(username.equals(enteredUsername)){
-            System.out.println("Enter the password");
-            enteredPassword = sc.next();
-            if(password.equals(enteredPassword)){
-                System.out.println("Successfully logged in");
-            }else{
-                System.out.println("Incorrect password");
-            }
-        }else{
-            System.out.println("Invalid username");
-        }
+        //1.)
+        String s = "The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.";
+        //Scanner sc = new Scanner(System.in);
+        String s1 = s.replace("fox", "frog");
+        System.out.println(s1);
 
+        //2.)
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Please enter a word to replace 'Fox' with: ");
+        String s2 = s.replace("fox", sc.next());
+        System.out.println(s2);
+
+        //3.)
+        System.out.println("Given the text, enter word you wish to replace: ");
+        String target = sc.next();
+        System.out.println("Enter String to replace the target: ");
+        String replace = sc.next();
+
+        String s3 = s.replace(target, replace);
+        System.out.println(s3);
     }
 
 }
