74e50abb6e15e1bc046603cd17aa0ae9fe5df1d6
Brad Reed <67254003+bushido1586@users.noreply.github.com>
2020-10-02 15:22:21 -0400

    problem 16 solution  Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,29 +1,11 @@
 package com.company;
-
-import java.util.Scanner;
+import demo.Link;
 
 public class Main {
 
     public static void main(String[] args)  {
-        //1.)
-        String s = "The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog. The quick brown fox jumps over the lazy dog.";
-        String s1 = s.replace("fox", "frog");
-        System.out.println(s1);
-
-        //2.)
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Please enter a word to replace 'Fox' with: ");
-        String s2 = s.replace("fox", sc.next());
-        System.out.println(s2);
-
-        //3.)
-        System.out.println("Given the text, enter word you wish to replace: ");
-        String target = sc.next();
-        System.out.println("Enter String to replace the target: ");
-        String replace = sc.next();
-
-        String s3 = s.replace(target, replace);
-        System.out.println(s3);
+        Link l = new Link();
+        l.displayLink();
     }
 
 }
---
+++ src/demo/Link.java
@@ -0,0 +1,8 @@
+package demo;
+
+public class Link {
+
+    public void displayLink(){
+        System.out.println("Link from the demo package");
+    }
+}
