f4b4c178551251737b07acefb921d1715ea39fbf
Brad Reed <67254003+bushido1586@users.noreply.github.com>
2020-10-02 17:15:01 -0400

    Problem 17 solution Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,15 +4,24 @@ import demo.Link;
 public class Main {
 
     public static void main(String[] args)  {
+        Customer c = new Customer();
+        c.deposit();
+        c.withdraw();
+        c.updateBalance();
 
-        Vehicle v = new Vehicle();
-        v.drive();
-        v.stop();
-
-        People p = new People();
-        p.eating();
-        p.playing();
 
     }
 
+    public static class Bank{
+        void deposit(){
+            System.out.println("Amount has been deposited");
+        }
+        void withdraw(){
+            System.out.println("Amount has been withdrawn");
+        }
+        void updateBalance(){
+            System.out.println("Balance has been updated");
+        }
+    }
+    public static class Customer extends Bank{}
 }
