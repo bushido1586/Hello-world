19974a5843fde32a8d052804892b0a9cf177bd88
Brad Reed <67254003+bushido1586@users.noreply.github.com>
2020-10-05 16:37:22 -0400

    ArrayList part one Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,14 +1,32 @@
 package com.company;
 import demo.Link;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args)  {
-        PrivateBank pb = new PrivateBank("John");
-        pb.interestPaid();
 
-        GovernmentBank gb = new GovernmentBank("Rob");
-        gb.interestPaid();
+        ArrayList<String> list = new ArrayList<String>();
+
+        list.add("cat");
+        list.add("jaguar");
+
+        //System.out.println(list.get(1));
+
+        list.set(1, "cheetah");
+
+      //  System.out.println(list.get(1));
+
+        if (list.contains("cat")){
+            System.out.println("It is present");
+        }else{
+            System.out.println("Not present");
+        }
+
+        for (int i=0; i< list.size(); i++){
+            System.out.println(list.get(i));
+        }
     }
 
 }
