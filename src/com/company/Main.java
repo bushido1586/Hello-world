9e614cc46f3e6261f69c299c1fbd6e825e867a23
Brad Reed <67254003+bushido1586@users.noreply.github.com>
2020-10-10 18:11:44 -0400

    multithreading in action part 3-join() Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,10 +2,12 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
 
         ThreadClass tc1 = new ThreadClass("2");
         tc1.t.start();
+        tc1.t.join();
+
         for (int i=0; i<5; i++){
             System.out.println("Main thread"+i);
             Thread.sleep(1000);
