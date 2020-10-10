cae5902d791b596689b4c1516b7249bf3e5df51b
Brad Reed <67254003+bushido1586@users.noreply.github.com>
2020-10-10 19:09:30 -0400

    multithreading yield method Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,16 +2,14 @@ package com.company;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
+        ThreadClass t = new ThreadClass();
+        t.start();
 
-        ThreadClass tc1 = new ThreadClass("2");
-        tc1.t.start();
-        tc1.t.join();
-
-        for (int i=0; i<5; i++){
-            System.out.println("Main thread"+i);
-            Thread.sleep(1000);
+        for (int i = 0; i < 5; i++) {
+            Thread.yield();
+            System.out.println(Thread.currentThread().getName() + " is running");
         }
-    }
 
+    }
 }
--- src/com/company/ThreadClass.java
+++ src/com/company/ThreadClass.java
@@ -1,23 +1,9 @@
 package com.company;
 
-public class ThreadClass implements Runnable {
-    String name;
-    Thread t;
-    ThreadClass(String tname){
-        name = tname;
-        t= new Thread(this, name);
-        System.out.println(t);
-    }
-
-    @Override
-    public void run() {
-        for (int i=0; i<5; i++){
-            System.out.println("second thread"+i);
-            try {
-                Thread.sleep(400);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
+public class ThreadClass extends Thread {
+    public void run(){
+        for (int i=0; i<5;i++){
+            System.out.println(Thread.currentThread().getName()+" is Running");
         }
     }
 }
