4865fe7af08b25d64862d16c2487ae9140b94bb4
Brad Reed <67254003+bushido1586@users.noreply.github.com>
2020-10-10 14:54:00 -0400

    Lambda Expressions part 1 Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,14 @@
 package com.company;
 
-import java.util.*;
-
 public class Main {
 
     public static void main(String[] args) {
-        Student s1 = new Student("Brad", 1, 85.6);
-        Student s2 = new Student("Kristin", 2, 95.4);
-        Student s3 = new Student("Paul", 3, 90.5);
-        Student s4 = new Student("Holly", 4, 92.1);
 
-        ArrayList<Student> roster = new ArrayList<>();
-        roster.add(s1);
-        roster.add(s2);
-        roster.add(s3);
-        roster.add(s4);
+        Number n;
 
-        for (Student student : roster) {
-            System.out.println(student.id + " " + student.name + " " + student.percentage);
-        }
+        n = () -> 200 * 100;
 
+        System.out.println(n.displayNumber());
 
     }
 
---
+++ src/com/company/Number.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public interface Number {
+
+    int displayNumber();
+
+}
