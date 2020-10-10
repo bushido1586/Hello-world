1eaa45bc3cbfaac4e7f5b3b46ce5cff2945d24ae
Brad Reed <67254003+bushido1586@users.noreply.github.com>
2020-10-10 15:17:46 -0400

    Lambda Expressions part 5(generics) Commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,17 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Person p;
-
-        p=(age)-> {
-            if(age>18){
-                System.out.println("Adult");
-            }else{
-                System.out.println("Not an adult");
-            }
+        Message<String> message;
+        message = (m)->{
+          return m;
         };
-
-        p.checkForAdult(29);
+        System.out.println(message.displayMessage("Hello There"));
     }
 
 }
---
+++ src/com/company/Message.java
@@ -0,0 +1,7 @@
+package com.company;
+
+public interface Message<T> {
+
+    T displayMessage(T message);
+
+}
